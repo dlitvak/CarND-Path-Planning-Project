267877f3867d5787bcc95eaa3c316ee7b64f50c0
Dmitriy Litvak <dlitvak@tibco.com>
2019-08-11 02:15:24 -0700

    Final code

--- src/spline.h
+++ src/spline.h
@@ -293,7 +293,7 @@ void spline::set_points(const std::vector<double>& x,
     // TODO: maybe sort x and y, rather than returning an error
     for(int i=0; i<n-1; i++) {
         if (m_x[i]>=m_x[i+1])
-            std::cout << "m_x[i]: " << m_x[i] << ", m_x[i+1]: " << m_x[i+1] << std::endl;
+            std::cout << "i: " << i << ", m_x[i]: " << m_x[i] << ", m_x[i+1]: " << m_x[i+1] << std::endl;
         assert(m_x[i]<m_x[i+1]);
     }
 
